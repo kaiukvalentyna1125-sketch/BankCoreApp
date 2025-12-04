735a3e88917c5e16f1963e7329463c84a8a7532a
valia <kaiuk.valentyna1125@vu.cdu.edu.ua>
2025-12-04 21:36:02 +0100

    initial commit

---
+++ src/main/java/org/bank/bankcoreapp/BankCoreAppApplication.java
@@ -0,0 +1,14 @@
+package org.bank.bankcoreapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCoreAppApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello! It is a new project!!!");
+        SpringApplication.run(BankCoreAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bank/bankcoreapp/BankCoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bank.bankcoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
